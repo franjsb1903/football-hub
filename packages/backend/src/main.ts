e46c820b81af2ade8d6fe6474295b6f501fcfcc3
Fran Saa <franchu.besteiro@gmail.com>
2025-05-10 12:57:29 +0200

    Implementar cors y prefijo de api

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core'
+import { Logger } from '@nestjs/common'
 
 import { AppModule } from './application'
 
+const CORS_ORIGINS = process.env.CORS_ORIGINS?.split(',') || []
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	app.setGlobalPrefix('api')
+	app.enableCors({
+		origin: CORS_ORIGINS, // Permitir todos los orígenes (no recomendado para producción)
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		allowedHeaders: 'Content-Type, Accept, Authorization',
+	})
+	const port = process.env.PORT ?? 3000
+	await app.listen(port)
+	app.enableShutdownHooks()
+
+	const logger = new Logger()
+	logger.log(`Application is running on: ${await app.getUrl()}`)
 }
 // eslint-disable-next-line unicorn/prefer-top-level-await
 bootstrap()
