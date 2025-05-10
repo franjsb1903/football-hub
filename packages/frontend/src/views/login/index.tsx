96d1f303735f7e320a077c7ce072893cc55d1577
Fran Saa <franchu.besteiro@gmail.com>
2025-05-10 17:09:51 +0200

    Añadir template para formulario de login

--- packages/frontend/src/views/login/index.tsx
+++ packages/frontend/src/views/login/index.tsx
@@ -1,12 +1,59 @@
+import type React from 'react'
+
+import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 import styles from './styles.module.css'
 
-export default function Login() {
+export default function LoginForm({
+	className,
+	...properties
+}: React.ComponentPropsWithoutRef<'div'>) {
 	return (
-		<section>
-			<h1 className={styles.some}>Login</h1>
-			<p>Login page</p>
-			<Button>Login</Button>
-		</section>
+		<div className={cn(styles.container, className)} {...properties}>
+			<Card>
+				<CardHeader>
+					<CardTitle className={styles.title}>Login</CardTitle>
+					<CardDescription>
+						Enter your email below to login to your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form>
+						<div className={styles.form}>
+							<div className={styles.field}>
+								<Label htmlFor="email">Email</Label>
+								<Input
+									id="email"
+									type="email"
+									placeholder="m@example.com"
+									required
+								/>
+							</div>
+							<div className={styles.field}>
+								<Label htmlFor="password">Password</Label>
+
+								<Input id="password" type="password" required />
+							</div>
+							<Button type="submit" className={styles.button}>
+								Login
+							</Button>
+						</div>
+						<div className={styles.register}>
+							Don&apos;t have an account?{' '}
+							<a href="/register">Sign up</a>
+						</div>
+					</form>
+				</CardContent>
+			</Card>
+		</div>
 	)
 }
