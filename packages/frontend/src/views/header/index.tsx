0ea180334c8768e6d29759e9db056738ae3b5f0f
Fran Saa <franchu.besteiro@gmail.com>
2025-05-11 20:47:57 +0200

    Añadir enlaces en el header y cambiar botón por icono

--- packages/frontend/src/views/header/index.tsx
+++ packages/frontend/src/views/header/index.tsx
@@ -1,22 +1,24 @@
 import { signOut } from 'next-auth/react'
+import Link from 'next/link'
+import { SlLogout as Logout } from 'react-icons/sl'
 
-import { Button } from '@/components/ui/button'
 import styles from './styles.module.css'
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
 			<h3>Football Hub</h3>
-			<section>
-				<Button
+			<section className={styles.links}>
+				<Link href="/fixtures">Partidos</Link>
+				<Link href="/teams">Mis equipos</Link>
+				<Logout
+					className={styles.button}
 					onClick={() =>
 						signOut({
 							redirect: false,
 						})
 					}
-				>
-					Cerrar sesión
-				</Button>
+				/>
 			</section>
 		</header>
 	)
