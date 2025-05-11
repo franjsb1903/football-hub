79b173a1993c603f144ab81275f600cf8d3c84ae
Fran Saa <franchu.besteiro@gmail.com>
2025-05-11 20:38:11 +0200

    Implementar botón para cerrar sesión

--- packages/frontend/src/views/header/index.tsx
+++ packages/frontend/src/views/header/index.tsx
@@ -1,9 +1,23 @@
+import { signOut } from 'next-auth/react'
+
+import { Button } from '@/components/ui/button'
 import styles from './styles.module.css'
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
 			<h3>Football Hub</h3>
+			<section>
+				<Button
+					onClick={() =>
+						signOut({
+							redirect: false,
+						})
+					}
+				>
+					Cerrar sesión
+				</Button>
+			</section>
 		</header>
 	)
 }
