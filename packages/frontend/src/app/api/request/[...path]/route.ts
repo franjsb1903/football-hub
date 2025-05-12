382023e4c89b4e4bf87eb742b84ce420db884810
Fran Saa <franchu.besteiro@gmail.com>
2025-05-12 20:59:39 +0200

    Gestionar errores de llamada a api

--- packages/frontend/src/app/api/request/[...path]/route.ts
+++ packages/frontend/src/app/api/request/[...path]/route.ts
@@ -10,20 +10,24 @@ export async function GET(
 	const { path } = await params
 
 	const urlPath = Array.isArray(path) ? path.join('/') : path
+	console.log({ urlPath })
 	const searchParameters = request.nextUrl.searchParams
 
 	const parameters = searchParameters.entries()
 	const requestParameters = buildParameters(parameters)
 
 	const finalPath = buildFinalPath(requestParameters, urlPath)
+	try {
+		const response = await api.get(finalPath, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
 
-	const response = await api.get(finalPath, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
-
-	return NextResponse.json(response)
+		return NextResponse.json(response)
+	} catch (error: any) {
+		return handleError(error)
+	}
 }
 
 export async function POST(
@@ -41,14 +45,17 @@ export async function POST(
 	const requestParameters = buildParameters(parameters)
 
 	const finalPath = buildFinalPath(requestParameters, urlPath)
+	try {
+		const response = await api.post(finalPath, body, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
 
-	const response = await api.post(finalPath, body, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
-
-	return NextResponse.json(response)
+		return NextResponse.json(response)
+	} catch (error) {
+		return handleError(error)
+	}
 }
 
 export async function DELETE(
@@ -65,14 +72,17 @@ export async function DELETE(
 	const requestParameters = buildParameters(parameters)
 
 	const finalPath = buildFinalPath(requestParameters, urlPath)
+	try {
+		const response = await api.delete(finalPath, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
 
-	const response = await api.delete(finalPath, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	})
-
-	return NextResponse.json(response)
+		return NextResponse.json(response)
+	} catch (error) {
+		return handleError(error)
+	}
 }
 
 function buildParameters(
@@ -93,4 +103,28 @@ function buildFinalPath(requestParameters: string[], urlPath: string) {
 	return requestParameters.length > 0
 		? `${urlPath}?${requestParameters.join('&')}`
 		: urlPath
+}
+
+function handleError(error: any) {
+	console.error('Error en la llamada a la API externa:', error)
+
+	let errorMessage = 'Ha ocurrido un error inesperado.'
+	let statusCode = 500
+
+	if (error.response) {
+		errorMessage =
+			error.response.data?.message || error.response.data || error.message
+		statusCode = error.response.status
+	} else if (error.request) {
+		errorMessage = 'No se recibió respuesta del servidor externo.'
+		statusCode = 503
+	} else if (error.message === 'Unauthorized') {
+		errorMessage = 'Unauthorized'
+		statusCode = 401
+	} else {
+		errorMessage = error.message
+		statusCode = 500
+	}
+
+	return NextResponse.json({ message: errorMessage }, { status: statusCode })
 }
