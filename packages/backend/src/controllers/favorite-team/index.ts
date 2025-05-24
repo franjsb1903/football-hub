4c9fb7c405660b2936265daa45687d448233a161
Fran Saa <franchu.besteiro@gmail.com>
2025-05-24 12:39:08 +0200

    Eliminar gestión de errores en controlador de equipos favoritos

--- packages/backend/src/controllers/favorite-team/index.ts
+++ packages/backend/src/controllers/favorite-team/index.ts
@@ -27,62 +27,47 @@ export default class FavoriteTeamController {
 
 	@Get()
 	async getAll(@Request() request) {
-		try {
-			const { id: userId } = request.user
+		const { id: userId } = request.user
 
-			const teams = await this.favoriteTeamProvider.getAllByUser(userId)
+		const teams = await this.favoriteTeamProvider.getAllByUser(userId)
 
-			return teams.map((team) => ({
-				id: team.teamId,
-				code: team.code,
-				country: team.country,
-				logo: team.teamLogo,
-				name: team.name,
-			}))
-		} catch (error) {
-			this.logger.error('Error getting favorite teams', error)
-			throw error
-		}
+		return teams.map((team) => ({
+			id: team.teamId,
+			code: team.code,
+			country: team.country,
+			logo: team.teamLogo,
+			name: team.name,
+		}))
 	}
 
 	@Post()
 	async saveFavoriteTeam(@Body() team: Team, @Request() request) {
-		try {
-			const { id: userId } = request.user
+		const { id: userId } = request.user
 
-			if (!team || !team.id) {
-				throw new BadRequestException('El equipo no es correcto')
-			}
-
-			const result = await this.favoriteTeamProvider.saveFavoriteTeam(
-				userId,
-				team,
-			)
-			return result
-		} catch (error) {
-			this.logger.error('Error saving favorite team', error)
-			throw error
+		if (!team || !team.id) {
+			throw new BadRequestException('El equipo no es correcto')
 		}
+
+		const result = await this.favoriteTeamProvider.saveFavoriteTeam(
+			userId,
+			team,
+		)
+		return result
 	}
 
 	@Delete('/:id')
 	async deleteFavoriteTeam(@Param('id') teamId: string, @Request() request) {
-		try {
-			const parsedTeamId = Number.parseInt(teamId)
-			if (Number.isNaN(parsedTeamId)) {
-				throw new BadRequestException('El id del equipo no es correcto')
-			}
-
-			const { id: userId } = request.user
-
-			const result = await this.favoriteTeamProvider.deleteFavoriteTeam(
-				userId,
-				parsedTeamId,
-			)
-			return result
-		} catch (error) {
-			this.logger.error('Error deleting favorite team', error)
-			throw error
+		const parsedTeamId = Number.parseInt(teamId)
+		if (Number.isNaN(parsedTeamId)) {
+			throw new BadRequestException('El id del equipo no es correcto')
 		}
+
+		const { id: userId } = request.user
+
+		const result = await this.favoriteTeamProvider.deleteFavoriteTeam(
+			userId,
+			parsedTeamId,
+		)
+		return result
 	}
 }
